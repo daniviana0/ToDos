b2216bac1739ffe9bce7bc67d31bf6071be68c0d
daniviana0 <danivianadecarvalho@gmail.com>
2026-03-17 10:49:52 -0300

    Codificação 6

---
+++ ToDoPlatform/Services/IUserService.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+using ToDoPlatform.ViewModels;
+
+namespace ToDoPlatform.Services;
+public interface IUserService
+{
+    Task<SignInResult> Login(LoginVM login);
+    Task Logout();
+}
